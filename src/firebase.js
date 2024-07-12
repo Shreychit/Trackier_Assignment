d234fab5ee37dac29b66102cec07153ebb872c2c
Shreychit <shreyaschit15@gmail.com>
2024-07-12 06:48:38 +0530

    configured firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
+
 import { initializeApp } from "firebase/app";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCotzC4gY_eSWXysBe6YOrwQNuxP4kOwHc",
+  authDomain: "trelloclone-8862c.firebaseapp.com",
+  projectId: "trelloclone-8862c",
+  storageBucket: "trelloclone-8862c.appspot.com",
+  messagingSenderId: "774235874773",
+  appId: "1:774235874773:web:0b27fcba3615438f94895c"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const fbFunctions = getFunctions(app);
